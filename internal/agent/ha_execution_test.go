0db1090fca3e761d397faeb7f0040991100806f0
polkiloo <michaellevitskiy@gmail.com>
2026-05-16 00:30:10 +0300

    test(agent): remove redundant loop variable copy

--- internal/agent/ha_execution_test.go
+++ internal/agent/ha_execution_test.go
@@ -1034,7 +1034,6 @@ func TestReplicaFollowHelpers(t *testing.T) {
 		}
 
 		for _, testCase := range tests {
-			testCase := testCase
 			t.Run(testCase.name, func(t *testing.T) {
 				t.Parallel()
 
